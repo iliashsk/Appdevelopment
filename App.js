da83be06e5bd1eb12706c3aaff59f55c46795a83
iliash <iliash.sk462@gmail.com>
2022-12-28 16:07:48 +0530

    App

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Cat from './src/Cat.js'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cat/>
+      <Text style={styles.hello}>
+      how are You My friends
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+    color:'goldenrod',
+  },
 });
+AppRegistry.registerComponent(
+  'MyReactNativeApp',
+  () => HelloWorld,
+);
